2eaba7846b0b49784b2353c3fc76e34a58a195cc
Eric Maino <ericmai@microsoft.com>
2021-03-21 16:13:02 -0700

    EXAMPLE: Reading applicaiton directory during startup

--- AzFuncLocalDirectory.cs
+++ AzFuncLocalDirectory.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Threading.Tasks;
+using AzFuncLocalDirectory;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -8,16 +9,23 @@ using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Function
 {
-    public static class AzFuncLocalDirectory
+    public class AzFuncLocalDirectory
     {
+        private readonly AzureFunctionsServiceConfig _service;
+
+        public AzFuncLocalDirectory(AzureFunctionsServiceConfig service)
+        {
+            _service = service;
+        }
+
         [FunctionName("AzFuncLocalDirectory")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log, ExecutionContext context)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            string responseMessage = requestBody + $". Current directory {context.FunctionAppDirectory}";
+            string responseMessage = requestBody + $". Current directory {_service.RootDirectory}";
 
             return new OkObjectResult(responseMessage);
         }
---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(AzFuncLocalDirectory.Startup))]
+
+namespace AzFuncLocalDirectory
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+            var services = builder.Services;
+            var context = builder.GetContext();
+            services.AddSingleton<AzureFunctionsServiceConfig>(new AzureFunctionsServiceConfig(context));
+        }
+    }
+    public class AzureFunctionsServiceConfig
+    {
+        public AzureFunctionsServiceConfig(FunctionsHostBuilderContext context)
+        {
+            RootDirectory = context.ApplicationRootPath;
+        }
+
+        public string RootDirectory { get; }
+    }
+
+}
